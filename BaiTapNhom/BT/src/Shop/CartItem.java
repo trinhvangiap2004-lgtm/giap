72fa1cf3425417a818ce6da8f7e2c901faee94aa
trinhvangiap2004-lgtm <trinhvangiap2004@gmail.com>
2025-12-11 17:52:43 +0700

    Delete BaiTapNhom directory

--- BaiTapNhom/BT/src/Shop/CartItem.java
+++
@@ -1,7 +0,0 @@
-package Shop;
-
-public class CartItem {
-    Product product;
-    int quantity;
-    public CartItem(Product product,int quantity){ this.product=product; this.quantity=quantity; }
-}
--- BaiTapNhom/BT/src/Shop/Product.java
+++
@@ -1,7 +0,0 @@
-package Shop;
-
-public class Product {
-    String id,name;
-    double price;
-    public Product(String id,String name,double price){ this.id=id; this.name=name; this.price=price; }
-}
